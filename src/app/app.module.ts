a424772720438ac01c1b4eb89cde33c5af32b7c4
Tiago Santini <tiagosantiniathayde@gmail.com>
2023-10-23 15:19:45 -0300

    add: templates iniciais

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { CoreModule } from './core/core.module';
+import { DashboardModule } from './views/dashboard/dashboard.module';
 
 @NgModule({
   declarations: [AppComponent],
@@ -17,6 +19,9 @@ import { MatCardModule } from '@angular/material/card';
 
     MatButtonModule,
     MatCardModule,
+
+    CoreModule,
+    DashboardModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
