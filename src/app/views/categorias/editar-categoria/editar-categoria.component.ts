ea17bcbb5592a5f385b0cc82e00b3d2f80055083
Tiago Santini <tiagosantiniathayde@gmail.com>
2023-10-25 14:13:29 -0300

    add: editar-categoria component

--- src/app/views/categorias/editar-categoria/editar-categoria.component.ts
+++ src/app/views/categorias/editar-categoria/editar-categoria.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { CategoriasService } from '../services/categorias.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Categoria } from '../models/categoria';
 
 @Component({
   selector: 'app-editar-categoria',
   templateUrl: './editar-categoria.component.html',
-  styleUrls: ['./editar-categoria.component.scss']
+  styleUrls: ['./editar-categoria.component.scss'],
 })
-export class EditarCategoriaComponent {
+export class EditarCategoriaComponent implements OnInit {
+  form?: FormGroup;
 
+  constructor(
+    private fb: FormBuilder,
+    private categoriasService: CategoriasService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.form = this.fb.group({
+      titulo: [''],
+    });
+
+    const categoria = this.route.snapshot.data['categoria'];
+    this.form.patchValue(categoria);
+  }
+
+  gravar(): void {
+    const id = parseInt(this.route.snapshot.paramMap.get('id')!);
+
+    this.categoriasService.editar(id, this.form?.value).subscribe({
+      next: (res) => this.processarSucesso(res),
+      error: (err) => this.processarFalha(err),
+    });
+  }
+
+  processarSucesso(res: Categoria) {
+    this.router.navigate(['/categorias', 'listar']);
+  }
+
+  processarFalha(err: any) {
+    console.error('Erro:', err);
+  }
 }
