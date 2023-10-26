5da1363a183dbfd7b82e49c98411fe86ea5d62ee
Tiago Santini <tiagosantiniathayde@gmail.com>
2023-10-26 15:38:28 -0300

    add: inserir-nota

--- src/app/views/notas/notas-routing.module.ts
+++ src/app/views/notas/notas-routing.module.ts
@@ -2,6 +2,8 @@ import { NgModule, inject } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ListarNotasComponent } from './listar-notas/listar-notas.component';
 import { NotasService } from './services/notas.service';
+import { InserirNotaComponent } from './inserir-nota/inserir-nota.component';
+import { listarCategoriasResolver } from '../categorias/services/listar-categorias.resolver';
 
 const listarNotasResolver = () => {
   return inject(NotasService).selecionarTodos();
@@ -18,10 +20,11 @@ const routes: Routes = [
     component: ListarNotasComponent,
     resolve: { notas: listarNotasResolver },
   },
-  // {
-  //   path: 'inserir',
-  //   component: InserirCategoriaComponent,
-  // },
+  {
+    path: 'inserir',
+    component: InserirNotaComponent,
+    resolve: { categorias: listarCategoriasResolver },
+  },
   // {
   //   path: 'editar/:id',
   //   component: EditarCategoriaComponent,
--- src/app/views/notas/notas.module.ts
+++ src/app/views/notas/notas.module.ts
@@ -6,12 +6,21 @@ import { SharedModule } from 'src/app/shared/shared.module';
 import { ReactiveFormsModule } from '@angular/forms';
 import { ListarNotasComponent } from './listar-notas/listar-notas.component';
 import { NotasService } from './services/notas.service';
+import { InserirNotaComponent } from './inserir-nota/inserir-nota.component';
+
+import { MatSelectModule } from '@angular/material/select';
+import { MatRadioModule } from '@angular/material/radio';
+import { CategoriasModule } from '../categorias/categorias.module';
 
 @NgModule({
-  declarations: [
-    ListarNotasComponent
+  declarations: [ListarNotasComponent, InserirNotaComponent],
+  imports: [
+    NotasRoutingModule,
+    SharedModule,
+    CategoriasModule,
+    MatSelectModule,
+    MatRadioModule,
   ],
-  imports: [NotasRoutingModule, SharedModule],
-  providers: [NotasService]
+  providers: [NotasService],
 })
 export class NotasModule {}
